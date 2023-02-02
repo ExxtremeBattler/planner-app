0f5f302d5e44253b781dad12e4092394de0aafca
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-02 10:46:00 +0000

    added div id's, currentHour to track hour

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+$(document).ready(function () {
+    
 let currentDay = document.getElementById("currentDay")
+var currentHour = moment().hour();
 
 function getCurrentDay () {
 
-    return moment().format('MMMM Do YYYY');
-    
+    return moment().format('MMMM Do YYYY');    
 }
 
 currentDay.append(getCurrentDay())
+console.log(currentHour);
+
+})
