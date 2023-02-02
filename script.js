3775c8e1778cef8a8bc5ccedd814f2fbf59beeac
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-02 11:00:59 +0000

    added logic to compute past, present, future

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     
 let currentDay = document.getElementById("currentDay")
 var currentHour = moment().hour();
+let timeblockArr = document.getElementsByClassName("timeblock")
 
 function getCurrentDay () {
 
@@ -10,5 +11,26 @@ function getCurrentDay () {
 
 currentDay.append(getCurrentDay())
 console.log(currentHour);
+console.log(timeblockArr[0].getAttribute('id'))
 
+timeblockArr.forEach(element => {
+    var timeblockHour = element.getAttribute('id')
+
+    if (timeblockHour < currentHour) {
+        element.classList.remove('present')
+        element.classList.remove('future')
+        element.classList.add('past')
+    }
+    else if (timeblockHour > currentHour) {
+        element.classList.remove('present')
+        element.classList.add('future')
+        element.classList.remove('past')
+    }
+    else {
+        element.classList.add('present')
+        element.classList.remove('future')
+        element.classList.remove('past')
+    }
+    
+});
 })
